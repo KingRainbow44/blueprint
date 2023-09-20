d1ce12aa6aafd557bb95bdf883e94ba3756c1784
KingRainbow44 <kobedo11@gmail.com>
2023-09-20 14:00:00 +0100

    Fetching modules & Move `fetch` to `utils.ts`

--- src/backend/canvas.ts
+++ src/backend/canvas.ts
@@ -8,41 +8,9 @@
 // This holds the backings for the UI to access.
 // All information here is cached.
 
+import { fetch as cache } from "@app/utils.ts";
+
 export default class Canvas {
-    /**
-     * Fetches a resource from the Canvas API.
-     * Uses the cache if possible.
-     *
-     * @param resource The resource to fetch.
-     * @param fetcher The function to fetch the resource.
-     * @private
-     */
-    private static async fetch<T>(
-        resource: string, fetcher: () => Promise<T>
-    ) {
-        // Check if the resource is cached.
-        const data = localStorage.getItem(resource);
-        if (data) {
-            const parsed = JSON.parse(data) as { _ttl?: number; };
-
-            // Check if the resource is expired.
-            if (parsed._ttl && parsed._ttl > Date.now()) {
-                delete parsed._ttl;
-                return parsed as T;
-            }
-        }
-
-        // Fetch the resource.
-        const fetched = await fetcher();
-        // Cache the resource.
-        localStorage.setItem(resource, JSON.stringify({
-            _ttl: Date.now() + 1000 * 60 * 60 * 24, // 1 day
-            ...fetched
-        }));
-
-        return fetched;
-    }
-
     /**
      * Fetches all courses.
      *
@@ -53,10 +21,12 @@ export default class Canvas {
     public static async getCourses(options?: {
         dash?: boolean;
         as?: Enrollment;
+        describe?: boolean;
     }): Promise<Course[] | DashboardCard[]> {
         const courses = options?.dash ?
-            await Canvas.fetch("dashCards", () => API.getDashboardCards()) :
-            await Canvas.fetch("courses", () => API.getCourses(options?.as));
+            await cache("dashCards", () => API.getDashboardCards()) :
+            await cache("courses",
+                () => API.getCourses(options?.as, options?.describe));
 
         // Convert to an array.
         if (typeof courses == "object") {
@@ -73,6 +43,38 @@ export default class Canvas {
      */
     public static async getCourse(id: number): Promise<Course | DashboardCard | undefined> {
         return (await Canvas.getCourses()).find(course => course.id == id);
+    }
+
+    /**
+     * Fetches the modules for a given course.
+     *
+     * @param course The course to fetch the modules for.
+     */
+    public static async getModules(course: Course): Promise<Module[]> {
+        const modules = await cache(`modules${course.id}`, () => API.getModules(course));
+
+        // Convert to an array.
+        if (typeof modules == "object") {
+            return Object.values(modules);
+        } else {
+            return modules;
+        }
+    }
+
+    /**
+     * Gets the items in a module.
+     *
+     * @param module The module to fetch the items for.
+     */
+    public static async getModuleItems(module: Module): Promise<ModuleItem[]> {
+        const items = await cache(`items${module.id}`, () => API.getModuleItems(module));
+
+        // Convert to an array.
+        if (typeof items == "object") {
+            return Object.values(items);
+        } else {
+            return items;
+        }
     }
 }
 
@@ -99,14 +101,39 @@ class API {
      * Fetches all courses for the current user.
      *
      * @param as The type of enrollment to fetch. Defaults to everything.
+     * @param describe Whether to fetch the course descriptions.
      */
-    public static async getCourses(as?: Enrollment): Promise<Course[]> {
+    public static async getCourses(as?: Enrollment, describe?: boolean): Promise<Course[]> {
         let route = "/courses?";
         if (as) {
             route += `enrollment_type=${as || ""}&`;
         }
+        if (describe) {
+            route += "include[]=syllabus_body&";
+        }
 
         const response = await API.call(route);
+        return await response.json();
+    }
+
+    /**
+     * Fetches the modules for a given course.
+     *
+     * @param course The course to fetch the modules for.
+     */
+    public static async getModules(course: Course): Promise<Module[]> {
+        const route = `/courses/${course.id}/modules?include[]=items`;
+        const response = await API.call(route);
+        return await response.json();
+    }
+
+    /**
+     * Fetches the items in a module.
+     *
+     * @param module The module to fetch the items for.
+     */
+    public static async getModuleItems(module: Module): Promise<ModuleItem[]> {
+        const response = await API.call(module.items_url);
         return await response.json();
     }
 
@@ -122,6 +149,7 @@ class API {
 // The following are the types of the Canvas API.
 
 export type Enrollment = "teacher" | "student" | "ta" | "observer" | "designer";
+export type ModuleItemType = "Page"  | "SubHeader" | "ExternalUrl" | "File" | "Discussion" | "Assignment" | "Quiz" | "ExternalTool";
 
 // https://canvas.instructure.com/doc/api/courses.html#Course
 export type Course = {
@@ -130,6 +158,41 @@ export type Course = {
     name: string;
     course_code: string;
     original_name: string;
+    syllabus_body: string; // This is HTML which should be rendered.
+};
+
+// https://canvas.instructure.com/doc/api/modules.html#Module
+export type Module = {
+    id: number;
+    workflow_state: "active" | "deleted";
+    position: number;
+    name: string;
+    items_count: number;
+    items_url: string;
+    items: ModuleItem[] | undefined;
+    state: "locked" | "unlocked" | "started" | "completed";
+};
+
+// https://canvas.instructure.com/doc/api/modules.html#ModuleItem
+export type ModuleItem = {
+    id: number;
+    module_id: number;
+    position: number;
+    title: string;
+    indent: number;
+    type: ModuleItemType;
+    content_id?: number;
+    html_url: string | null;
+    url?: string; // This is the Canvas API object URL.
+    page_url?: string;
+    external_url?: string;
+    new_tab?: boolean;
+    content_details: {
+        points_possible: number;
+        due_at: string | null;
+        unlock_at: string | null;
+        lock_at: string | null;
+    };
 };
 
 // No documentation. This is inferred from the API response.
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,38 @@
 /**
+ * Fetches a resource from the Canvas API.
+ * Uses the cache if possible.
+ *
+ * @param resource The resource to fetch.
+ * @param fetcher The function to fetch the resource.
+ * @private
+ */
+export async function fetch<T>(
+    resource: string, fetcher: () => Promise<T>
+) {
+    // Check if the resource is cached.
+    const data = localStorage.getItem(resource);
+    if (data) {
+        const parsed = JSON.parse(data) as { _ttl?: number; };
+
+        // Check if the resource is expired.
+        if (parsed._ttl && parsed._ttl > Date.now()) {
+            delete parsed._ttl;
+            return parsed as T;
+        }
+    }
+
+    // Fetch the resource.
+    const fetched = await fetcher();
+    // Cache the resource.
+    localStorage.setItem(resource, JSON.stringify({
+        _ttl: Date.now() + 1000 * 60 * 60 * 24, // 1 day
+        ...fetched
+    }));
+
+    return fetched;
+}
+
+/**
  * Gets the color of a string.
  *
  * @param string The string to get the color of.
